e55f88e16838c4ee08ec6c3ac7e47e013efd2dea
Roxanna De Freitas <68925112+roxanna-df@users.noreply.github.com>
2021-11-04 12:29:36 +0000

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,53 @@
+var a= '';
+var b='';
+var num= [];
+var ans;
+
+//All the numbers and operators input will be stored in a array "num" using function "sendNum()"
+
+function sendNum(digit){
+
+    num.push(digit);
+
+    if(num.length !=1){
+        a='';
+        document.getElementById('screen').innerHTML= a;
+    }
+
+    for(i=0; i<num.length; i++){
+        
+        a= a+num[i];
+    }
+
+    document.getElementById('screen').innerHTML= a;
+}
+
+function equalTo(){
+    document.getElementById('screen').innerHTML= '';
+
+    for(i=0; i<num.length; i++){
+        b += num[i];
+    }
+
+    ans=eval(b);
+
+    document.getElementById('screen').innerHTML= ans;
+
+    while(num.length > 0){
+        num.pop();  //emptying the array "num"
+    }
+
+    num.push(ans.toString());
+
+}
+
+function clearScr(){
+    document.getElementById('screen').innerHTML= '';
+
+    while(num.length >0){
+        num.pop();
+    }
+
+    a='';
+    b='';
+}
